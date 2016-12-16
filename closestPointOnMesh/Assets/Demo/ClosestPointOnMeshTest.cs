2c55dbdfbf268857d62ca4d58e0ffc849c86294e
Vili <kobilica>
2016-12-16 11:28:08 +0100

    Updated demo scene, not finished yet

---
+++ closestPointOnMesh/Assets/Demo/ClosestPointOnMeshTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class ClosestPointOnMeshTest : MonoBehaviour {
+
+    [Range(0.01f, 100f)]
+    public float QueryRadius = 15f;
+
+	void Start () {
+	    	
+	}
+	
+	void Update () {
+
+        Vector3 pos = transform.position;
+
+        Collider[] colls = Physics.OverlapSphere(pos, QueryRadius);
+
+
+	}
+}
